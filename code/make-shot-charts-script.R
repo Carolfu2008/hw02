6fe3224abfbc85786540252ac7db569da90eac66
Carol <carol2008.berkeley.edu>
2018-03-09 14:14:47 -0800

    revise title and refresh content

--- code/make-shot-charts-script.R
+++ code/make-shot-charts-script.R
@@ -1,128 +1,155 @@
 # Title: hw02 GSW Shot Charts
-# Description:
-# purpose of hw02 assignment:
-# 1. using the packages dplyr and ggplot2 to work with data frame
-# 2. visualizing shot data of NBA players.
-# 3. producing so-called "shot charts" for a handful of players from the Golden State Warriors, as well as other summaries and visualizations.
-# Input(s): data file "nba2017-players.csv"
-# Output(s): Shot Charts
+# Description: Shot chart for NBA player
+# 1. make shot chart for 5 NBA players
+# 2. create facet chart for player
+# 3. export charts.
+# Input(s): data file "andre-iguodala.csv","draymond-green.csv", "kevin-durant.csv", "klay-thompson.csv", "stephen-curry.csv"
+# Output(s):  "andre-iguodala-shot-chart.pdf","draymond-green-shot-chart.pdf","gsw-shot-charts.pdf","kevin-durant-shot-chart.pdf","klay-thompson-shot-chart.pdf","stephen-curry-shot-chart.pdf"
 # Author(s): Huiqing Fu
 # Date: 03-01-2018
+
 # packages
 library(readr)    # importing data
 library(dplyr)    # data wrangling
 library(ggplot2)  # graphics
 
+## 4) Shot Charts (20 pts)
 ####4.1) Shot charts of each player (10 pts)
-Create shot charts (with court backgrounds) for each player, and save the plots in PDF
-format, with dimensions width = 6.5 and height = 5 inches, inside the folder images/:
-  ¡E andre-iguodala-shot-chart.pdf
-¡E draymond-green-shot-chart.pdf
-¡E kevin-durant-shot-chart.pdf
-¡E klay-thompson-shot-chart.pdf
-¡E stephen-curry-shot-chart.pdf
-```{r}
 
-#court_file <- "../images/nba-court.jpg"
-court_image <- rasterGrob(readJPEG("../images/nba-court.jpg"),
-                          width = unit(1,"npc"),
-                          height = unit(1,"npc")
+  # scatterplot
+  thompson_scatterplot <- ggplot(data = thompson) +
+    geom_point(aes(x = x, y = y, color = shot_made_flag))
+  thompson_scatterplot
 
-)
-iguodala_shot_chart <- ggplot(data = iguodala, aes(x = x, y =y))+
-  annotation_custom(court_image, -250,250, -50, 420)+
-  geom_point(aes(color=shot_made_flag))+
-  ylim(-50,420)+
-  ggtitle('Shot Chart: Andre Iguodala (2016 Season)')+
-  theme_minimal()
-iguodala_shot_chart
 
-```
-```{r}
-court_file <- "../images/nba-court.jpg"
-court_image <- rasterGrob(
-  readJPEG(court_file),
-  width = unit(1, "npc"),
-  height = unit(1, "npc"))
-green_shot_chart <- ggplot(data = green, aes(x = x, y = y, color = shot_made_flag))+
-  annotation_custom(court_image,  -250, 250, -50,420)+
-  ylim(-50,420)+
-  geom_point()+
-  ggtitle('Shot Chart: Draymond Green (2016 Season)')+
-  theme_minimal()
-green_shot_chart
-```
+  #The JPG file with this image is available in the course github repository, inside the folder images/. You need to import the file nba-court.jpg to the images/ folder of your HW directory.
+  # cd ../images
+  #curl -O https://raw.githubusercontent.com/ucb-stat133/stat133-spring-2018/master/data/nba-court.jpg
 
-```{r}
-curry_shot_chart <- ggplot(data = curry, aes(x = x, y = y, color = shot_made_flag))+
-  annotation_custom(court_image, -250,250,-50,420)+
-  geom_point()+
-  ylim(-50,420)+
-  ggtitle("Shot Chart: Stephen Curry (2016 Season)")+
-  theme_minimal()
-curry_shot_chart
-```
-```{r}
-durant_shot_chart <- ggplot(data = durant, aes(x = x, y = y, color = shot_made_flag))+
-  annotation_custom(court_image, -250, 250, -50, 420)+
-  geom_point()+
-  ylim(-50,420)+
-  ggtitle("Shot Chart: Kevin Durant (2016 Season)")+
-  theme_minimal()
-durant_shot_chart
-```
-```{r}
-thompson_shot_chart <- ggplot(data = thompson, aes(x = x, y = y, color = shot_made_flag))+
-  annotation_custom(court_image, -250, 250, -50, 420)+
-  geom_point()+
-  ylim(-50,420)+
-  ggtitle("Shot Chart: Klay Thompson (2016 Season)")+
-  theme_minimal()
-thompson_shot_chart
-```
-```{r}
-pdf(file = "../images/andre-iguodala-shot-chart.pdf", width = 7, height = 5)
-iguodala_shot_chart
-dev.off()
+  # court image (to be used as background of plot)
+  court_file <- "../images/nba-court.jpg"
+  # create raste object
 
-pdf(file = "../images/draymond-green-shot-chart.pdf", width = 7, height = 5)
-green_shot_chart
-dev.off()
+  court_image <- rasterGrob(
+    readJPEG(court_file),
+    width = unit(1, "npc"),
+    height = unit(1, "npc"))
 
-pdf(file = "../images/kevin-durant-shot-chart.pdf", width = 7, height = 5)
-durant_shot_chart
-dev.off()
+  # shot chart with court background
+  thompson_shot_chart <- ggplot(data = thompson) +
+    annotation_custom(court_image, -250, 250, -50, 420) +
+    geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+    ylim(-50, 420) +
+    ggtitle('Shot Chart: Klay Thompson (2016 season)') +
+    theme_minimal()
+  thompson_shot_chart
 
-pdf(file = "../images/klay-thompson-shot-chart.pdf", width = 7, height = 5)
-thompson_shot_chart
-dev.off()
+  ####4.1) Shot charts of each player (10 pts)
 
-pdf(file = "../images/stephen-curry-shot-chart.pdf", width = 7, height = 5)
-curry_shot_chart
-dev.off()
-```
+ # ¡E stephen-curry-shot-chart.pdf
 
-####4.2) Facetted Shot Chart (10 pts)
-Create one graph, using facetting, to show all the shot charts in one image, similar to the one
-below. Save this image in PDF format as gsw-shot-charts.pdf, inside the folder images/.
-Specify image dimensions width = 8 and height = 7 inches.
-```{r}
-gsw_shot_charts<- ggplot(data = dat, aes(x = x, y =y, color = shot_made_flag))+
-  annotation_custom(court_image, -250, 250, -50, 420)+
-  geom_point(size = 0.7)+
-  ylim(-50, 420)+
-  facet_wrap(~player)+
-  ggtitle("Shot Charts: GSW (2016 season)")+
-  theme_minimal()+
-  theme(legend.position="top")+
-  theme(legend.title = element_blank())+
-  #theme(legend.position = c(0.8, 0.3))+
-  theme(legend.text = element_text(size=8))
-gsw_shot_charts
-```
-```{r}
-pdf("../images/gsw-shot-charts.pdf", width = 8, height = 7)
-gsw_shot_charts
+  #```{r iguodala_shot_chart}
 
-dev.off()
-```
+  #court_file <- "../images/nba-court.jpg"
+  court_image <- rasterGrob(readJPEG("../images/nba-court.jpg"),
+                            width = unit(1,"npc"),
+                            height = unit(1,"npc")
+
+  )
+  iguodala_shot_chart <- ggplot(data = iguodala, aes(x = x, y =y))+
+    annotation_custom(court_image, -250,250, -50, 420)+
+    geom_point(aes(color=shot_made_flag))+
+    ylim(-50,420)+
+    ggtitle('Shot Chart: Andre Iguodala (2016 Season)')+
+    theme_minimal()
+  iguodala_shot_chart
+
+
+ # ```{r green_shot_chart}
+  court_file <- "../images/nba-court.jpg"
+  court_image <- rasterGrob(
+    readJPEG(court_file),
+    width = unit(1, "npc"),
+    height = unit(1, "npc"))
+  green_shot_chart <- ggplot(data = green, aes(x = x, y = y, color = shot_made_flag))+
+    annotation_custom(court_image,  -250, 250, -50,420)+
+    ylim(-50,420)+
+    geom_point()+
+    ggtitle('Shot Chart: Draymond Green (2016 Season)')+
+    theme_minimal()
+  green_shot_chart
+
+
+  #```{r curry_shot_chart}
+  curry_shot_chart <- ggplot(data = curry, aes(x = x, y = y, color = shot_made_flag))+
+    annotation_custom(court_image, -250,250,-50,420)+
+    geom_point()+
+    ylim(-50,420)+
+    ggtitle("Shot Chart: Stephen Curry (2016 Season)")+
+    theme_minimal()
+  curry_shot_chart
+
+ # ```{r durant_shot_chart}
+  durant_shot_chart <- ggplot(data = durant, aes(x = x, y = y, color = shot_made_flag))+
+    annotation_custom(court_image, -250, 250, -50, 420)+
+    geom_point()+
+    ylim(-50,420)+
+    ggtitle("Shot Chart: Kevin Durant (2016 Season)")+
+    theme_minimal()
+  durant_shot_chart
+
+ # ```{r thompson_shot_chart}
+  thompson_shot_chart <- ggplot(data = thompson, aes(x = x, y = y, color = shot_made_flag))+
+    annotation_custom(court_image, -250, 250, -50, 420)+
+    geom_point()+
+    ylim(-50,420)+
+    ggtitle("Shot Chart: Klay Thompson (2016 Season)")+
+    theme_minimal()
+  thompson_shot_chart
+
+ # ```{r export}
+  pdf(file = "../images/andre-iguodala-shot-chart.pdf", width = 7, height = 5)
+  iguodala_shot_chart
+  dev.off()
+
+  pdf(file = "../images/draymond-green-shot-chart.pdf", width = 7, height = 5)
+  green_shot_chart
+  dev.off()
+
+  pdf(file = "../images/kevin-durant-shot-chart.pdf", width = 7, height = 5)
+  durant_shot_chart
+  dev.off()
+
+  pdf(file = "../images/klay-thompson-shot-chart.pdf", width = 7, height = 5)
+  thompson_shot_chart
+  dev.off()
+
+  pdf(file = "../images/stephen-curry-shot-chart.pdf", width = 7, height = 5)
+  curry_shot_chart
+  dev.off()
+
+
+  ####4.2) Facetted Shot Chart (10 pts)
+  #Create one graph, using facetting, to show all the shot charts in one image, similar to the one
+ # below. Save this image in PDF format as gsw-shot-charts.pdf, inside the folder images/.
+  #Specify image dimensions width = 8 and height = 7 inches.
+
+  gsw_shot_charts<- ggplot(data = dat, aes(x = x, y =y, color = shot_made_flag))+
+    annotation_custom(court_image, -250, 250, -50, 420)+
+    geom_point(size = 0.7)+
+    ylim(-50, 420)+
+    facet_wrap(~player)+
+    ggtitle("Shot Charts: GSW (2016 season)")+
+    theme_minimal()+
+    theme(legend.position="top")+
+    theme(legend.title = element_blank())+
+    #theme(legend.position = c(0.8, 0.3))+
+    theme(legend.text = element_text(size=8))
+  gsw_shot_charts
+
+
+  pdf("../images/gsw-shot-charts.pdf", width = 8, height = 7)
+  gsw_shot_charts
+
+  dev.off()
+
